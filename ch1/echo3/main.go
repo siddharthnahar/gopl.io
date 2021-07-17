13ffe3b3b22de9f0898386e5e9d9169b6b2fa101
Siddharth Nahar <siddharth.nahar.7@gmail.com>
2021-07-17 11:43:33 +0100

    ch1 prac

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -1,8 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 8.
-
 // Echo3 prints its command-line arguments.
 package main
 
@@ -12,9 +7,6 @@ import (
 	"strings"
 )
 
-//!+
 func main() {
 	fmt.Println(strings.Join(os.Args[1:], " "))
 }
-
-//!-
